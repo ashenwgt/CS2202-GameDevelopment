970006c4114e3effe5e1107fdb1850a1af88d87e
Thilina Ashen Gamage <ashen.workplace@gmail.com>
2016-03-26 00:50:34 +0530

    created a parameter ballx, added getters for dy dx X BallX

--- GameProject/src/Ball.java
+++ GameProject/src/Ball.java
@@ -2,7 +2,8 @@ public class Ball extends GameEntity{
       
     private int dx;
     private int dy;
-
+    private static int ballX;
+    
     public Ball(int gameWidth, int gameHeight,int x, int y, int size,int speed) {
         super(gameWidth,gameHeight,x,y,size,size,speed);        
         dx=speed;
@@ -16,7 +17,8 @@ public class Ball extends GameEntity{
         if(y>(gameHeight-w) || y<0)
             dy*=-1;
         x+=dx;
-        y+=dy;          
+        y+=dy;  
+        ballX = x;
     }
 
     public int getSize() {
@@ -25,5 +27,21 @@ public class Ball extends GameEntity{
     
     public void reverse(){
         dy=-dy;
+    }
+    
+    public int getdy(){
+        return dy;
+    }
+    
+    public int getdx(){
+        return dx;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public static int getBallX(){
+        return ballX;
     }
 }
