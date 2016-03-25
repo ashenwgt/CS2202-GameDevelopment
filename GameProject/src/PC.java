8556344ade6db176d03bbba9884882fe6957cd6d
Thilina Ashen Gamage <ashen.workplace@gmail.com>
2016-03-26 00:46:11 +0530

    fixed issues in PC.update(), PC's bat moves with Ball's x-cordinate

--- GameProject/src/PC.java
+++ GameProject/src/PC.java
@@ -1,33 +1,20 @@
 public class PC extends GameEntity {
     
-    private int dx;
-    
+    private int dx;   
 
     public PC(int gameWidth, int gameHeight,int x, int y, int w,int h,int speed) {
         super(gameWidth,gameHeight,x,y,w,h,speed);
-        
+        dx=speed;
     }
     
     @Override
     public void update(){
-        //check the edges
-       x=x+dx;
-        if(x<=0){
-           x=0;
-       }
-       if(x>gameWidth-w){
-           x=gameWidth-w;
-       }
-       dx=0;
+        //check the edges    
+        if(x>(gameWidth-w) || x<0)
+            dx*=-1; 
+        // PC's bat always changes its position with Ball's x-cordinate, so PC never loses 
+        x = Ball.getBallX();
     }
    
-    public void setLeft(){
-        this.dx=-this.speed;
-       
-    }
-    
-    public void setRight(){
-        this.dx=this.speed;
-    }
     
 }
